9652fcebdd9b5d3282f23d4cc08481e49d5fb27c
apurplefrog <theodorebaker@gmail.com>
2025-02-02 21:20:04 -0500

    aerial.nvim | fidget.nvim | treesj.nvim | visual-multi.nvim

--- home-manager/programs/nvim/plugins/default.nix
+++ home-manager/programs/nvim/plugins/default.nix
@@ -6,8 +6,10 @@
   ];
 
   programs.nixvim.plugins = {
+    aerial.enable = true;
     bufferline.enable = true;
     comment.enable = true;
+    fidget.enable = true;
     image.enable = true;
     lazygit.enable = true;
     lualine.enable = true;
@@ -16,6 +18,8 @@
     quickmath.enable = true;
     render-markdown.enable = true;
     treesitter.enable = true;
+    treesj.enable = true;
+    visual-multi.enable = true;
     web-devicons.enable = true;
     yazi.enable = true;
   };
