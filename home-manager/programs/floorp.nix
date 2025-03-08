3bfbb05c2c40ded37a3a18b2107a82e8fb0afc17
autumn-the-kitty-cat <theodorebaker08@gmail.com>
2025-03-07 20:30:09 -0500

    create floorp.nix

---
+++ home-manager/programs/floorp.nix
@@ -0,0 +1,73 @@
+{pkgs, ...}: 
+let
+  betterfox = pkgs.fetchFromGitHub {
+    owner = "yokoffing";
+    repo = "Betterfox";
+    rev = "116.1";
+    hash = "sha256-Ai8Szbrk/4FhGhS4r5gA2DqjALFRfQKo2a/TwWCIA6g=";
+  };
+in
+{
+  programs.floorp = {
+    enable = true;
+    policies = {
+      DisableTelemtry = true;
+      DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value= true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never";
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified"; # alternative: "separate"
+        ExtensionSettings = {
+          # uBlock Origin:
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          "sponsorBlocker@ajay.app" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorBlocker@ajay.app/latest.xpi"; 
+            installation_mode = "force_installed";
+          };
+
+          "languagetool-webextension@languagetool.org" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/languagetool-webextension@languagetool.org/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Return dislikes to YouTube
+          "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/{762f9885-5a13-4abd-9c77-433dcd38b8fd}/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Catppuccin Mocha Mauve
+          "{76aabc99-c1a8-4c1e-832b-d4f2941d5a7a}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/catppuccin-mocha-mauve-git/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+    };
+
+    profiles.default = {
+      id = 0;
+      isDefault = true;
+      extraConfig = builtins.concatStringsSep "\n" [
+        (builtins.readFile "${betterfox}/Securefox.js")
+        (builtins.readFile "${betterfox}/Fastfox.js")
+        (builtins.readFile "${betterfox}/Peskyfox.js")
+      ];
+    };
+  };
+}
